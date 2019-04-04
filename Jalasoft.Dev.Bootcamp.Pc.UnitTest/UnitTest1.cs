2c3821c92af024ac5b3c2ceef45fd42622c80497
miv1 <m.iriarte.val@gmail.com>
2019-04-04 19:04:14 -0400

    new testing

---
+++ Jalasoft.Dev.Bootcamp.Pc.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Jalasoft.Dev.Bootcamp.Pc.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using Microsoft.VisualBasic.TestTools.UnitTesting;
+using Jalasoft.DevBootcamp.Pc;
+
+namespace XUnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
